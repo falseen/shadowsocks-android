65e696639a1f05ace1a4c249d62209057f9b5818
Falseen <hurxyill@163.com>
2017-01-17 21:53:12 +0800

    add lib to qpython32 and fix python32.sh

--- src/main/assets/qpython32/bin/python32.sh
+++ src/main/assets/qpython32/bin/python32.sh
@@ -1,7 +1,8 @@
 #!/system/bin/sh
 
 export PYTHONHOME=/data/data/com.github.shadowsocks/qpython32
-export PYTHONPATH=$PYTHONHOME/lib/python3.2/lib-dynload:$PYTHONHOME/lib/python3.2
+export PYTHONPATH=$PYTHONHOME/lib/python3.2/lib-dynload:$PYTHONHOME/lib/python3.2:$PYTHONHOME/site-package:$PYTHONHOME/python32.zip
 export PATH=$PYTHONHOME/bin:$PATH
-export LD_LIBRARY_PATH=$PYTHONHOME/lib:$LD_LIBRARY_PATH
-$PYTHONHOME/bin/python "$@"
+export ANDROID_PRIVATE=$PYTHONHOME
+export LD_LIBRARY_PATH=$PYTHONHOME/lib:$PYTHONHOME
+$PYTHONHOME/bin/python-android5 "$@"
