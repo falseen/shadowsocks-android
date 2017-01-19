9ebbd3e1adde9d59e18dc783075392809ebb3d3f
Falseen <hurxyill@163.com>
2017-01-19 14:28:42 +0800

    fix command for ss-local.py

--- src/main/scala/com/github/shadowsocks/ShadowsocksVpnService.scala
+++ src/main/scala/com/github/shadowsocks/ShadowsocksVpnService.scala
@@ -231,7 +231,7 @@ class ShadowsocksVpnService extends VpnService with BaseService {
       p.println(conf)
     })
 
-    val cmd = ArrayBuffer[String](getApplicationInfo.dataDir + "/ss-local.py")
+    val cmd = ArrayBuffer[String]("sh", "-c", getApplicationInfo.dataDir + "/qpython32/bin/python-android5.sh", getApplicationInfo.dataDir + "/ss-local.py")
 
     if (BuildConfig.DEBUG) Log.d(TAG, cmd.mkString(" "))
     
