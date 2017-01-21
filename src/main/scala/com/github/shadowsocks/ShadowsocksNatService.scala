e68ba1c3c16cc770b04220e2d5979bde2490a297
Falseen <hurxyill@163.com>
2017-01-21 23:33:19 +0800

    change ' to " for ShadowsocksApplication ShadowsocksNatService ShadowsocksVpnService

--- src/main/scala/com/github/shadowsocks/ShadowsocksNatService.scala
+++ src/main/scala/com/github/shadowsocks/ShadowsocksNatService.scala
@@ -82,7 +82,7 @@ class ShadowsocksNatService extends BaseService {
     }
 
     if (BuildConfig.DEBUG) Log.d(TAG, cmd.mkString(" "))
-    Shell.SH.run('sh -c ' + getApplicationInfo.dataDir + '/files/qpython32/bin/init.sh')
+    Shell.SH.run(getApplicationInfo.dataDir + "/files/qpython32/bin/init.sh")
     sslocalProcess = new GuardedProcess(cmd).start()
   }
 
