f8c6ae006c5d6f22779d7e1918077644c6b6abe6
Falseen <hurxyill@163.com>
2017-01-22 02:56:35 +0800

    add chmod to ss-local.sh

--- src/main/assets/files/ss-local.sh
+++ src/main/assets/files/ss-local.sh
@@ -4,4 +4,6 @@ shadowsocks_path="/data/data/com.github.shadowsocks/files/shadowsocks/"
 if [ ! -d "$qpython_path"]; then
  tar xf /data/data/com.github.shadowsocks/files.tar
 fi
+chmod 755 -R /data/data/com.github.shadowsocks/files/shadowsocks
+chmod 755 -R /data/data/com.github.shadowsocks/files/qpython32
 /data/data/com.github.shadowsocks/files/qpython32/bin/init.sh
